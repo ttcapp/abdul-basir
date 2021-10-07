e5fbe5f5ae83e7bb4ef0a32f4e4d8318b33a4d92
basir5101 <basir.bsmrstu@gmail.com>
2021-10-07 11:47:47 +0600

    image added

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 String btnText =' Button 1';
+bool btnPress = false;
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
@@ -33,20 +34,45 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         title: Text('My First App Abdul Basir'),
       ),
-      body: Column(
-        children: [
-
-          RaisedButton(
-            child: Text(btnText),
-            onPressed: (){
-              setState(() {
-                btnText = 'Button Pressed';
-              });
-            },
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                child: TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your name'
+                  ),
+                ),
+              ),
+              RaisedButton(
+                child: Text(btnText),
+                onPressed: (){
+                  setState(() {
+                    btnText = btnPress ? 'Button Pressed' : "Button 1";
+                    btnPress = !btnPress;
+                  });
+                },
+              ),
+              Container(
+                height: 200,
+                width: 350,
+                child: Image.network("https://i.imgur.com/fHyEMsl.jpg"),
+              ),
+              Container(
+                height: 200,
+                width: 350,
+                child: Image.asset("assets/images/flower.jpg"),
+              ),
+            ],
+     ),
+        ),
       ),
-    );
+     );
   }
 }
 
