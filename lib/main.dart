7dd6e9407a460ac8e5bc6f254c74be12872ea14c
basir5101 <basir.bsmrstu@gmail.com>
2021-10-10 12:30:05 +0600

    10/1/2021 img style

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,10 @@ String btnText2 =' Button 2';
 Color btnColor1 = Colors.purpleAccent;
 bool imgVisible1 = false;
 bool imgVisible2 = false;
+String imgSrc1 = "https://i.imgur.com/fHyEMsl.jpg";
+double imgHeight = 150;
+double imgWidth = 240;
+
 
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -40,12 +44,88 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Abdul Basir'),
       ),
       body: SingleChildScrollView(
+
         child: Center(
+
           child: Column(
             children: [
               SizedBox(
                 height: 15,
               ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage(imgSrc1), fit: BoxFit.cover),
+                          border: Border.all(color: Colors.purple, width: 4),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                          height: imgHeight,
+                          width: imgWidth,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage(imgSrc1), fit: BoxFit.cover),
+                          border: Border.all(color: Colors.purple, width: 4),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                        height: imgHeight,
+                        width: imgWidth,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage(imgSrc1), fit: BoxFit.cover),
+                          border: Border.all(color: Colors.purple, width: 4),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                        height: imgHeight,
+                        width: imgWidth,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage(imgSrc1), fit: BoxFit.cover),
+                          border: Border.all(color: Colors.purple, width: 4),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                        height: imgHeight,
+                        width: imgWidth,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: NetworkImage(imgSrc1), fit: BoxFit.cover),
+                          border: Border.all(color: Colors.purple, width: 4),
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                        height: imgHeight,
+                        width: imgWidth,
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+
+
+
+
+
+
               RaisedButton(
                 child: Text(btnText1),
                 color: btnColor1,
@@ -63,19 +143,22 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: 200,
                   width: 350,
                   decoration: BoxDecoration(color: Colors.purple[300]),
-                  child: Image.network("https://i.imgur.com/fHyEMsl.jpg"),
+                  child: Image.network(imgSrc1),
                 ),
               ),
-              RaisedButton(
-                child: Text(btnText2),
-                color: btnColor1,
-                onPressed: (){
-                  setState(() {
-                    btnText2 = 'Button Pressed';
-                    btnColor1 = Colors.deepPurpleAccent;
-                    imgVisible2 = true;
-                  });
-                },
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  child: Text(btnText2),
+                  color: btnColor1,
+                  onPressed: (){
+                    setState(() {
+                      btnText2 = 'Button Pressed';
+                      btnColor1 = Colors.deepPurpleAccent;
+                      imgVisible2 = true;
+                    });
+                  },
+                ),
               ),
               Visibility(
                 visible: imgVisible2,
